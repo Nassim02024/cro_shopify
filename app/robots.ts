a01b3e791bb8d9df73e66a1b86e096051f8a3a90
Nassim <nn1509789@gmail.com>
2026-02-19 12:36:28 +0100

    goog

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/', // الصفحات التي لا تريد ظهورها في البحث
     },
-    sitemap: 'flaylor-agency.online/sitemap.xml',
+    sitemap: 'https://flaylor-agency.online/sitemap.xml',
   }
 }
