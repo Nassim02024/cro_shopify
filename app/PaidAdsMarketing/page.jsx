0bc235bae6075d69a1df19021b8da8b87c4059d2
Nassim <nn1509789@gmail.com>
2026-03-13 14:53:29 +0100

    chan

--- app/PaidAdsMarketing/page.jsx
+++ app/PaidAdsMarketing/page.jsx
@@ -44,8 +44,8 @@ const PaidAdsLanding = () => {
 
     <main className="w-full pt-10">
       {/* Hero Section */}
-      <section className="flex flex-col items-center text-center  md:space-y-8 w-full px-6 md:px-[10%] lg:px-[20%]">
-        <div className='w-full mt-20'>
+      <section className="flex flex-col items-center text-center space-y-10 md:space-y-8 w-full px-6 md:px-[10%] lg:px-[20%]">
+        <div className='w-full mt-20 '>
           {/* تعديل الخط ليكون مرناً */}
           <h1 className="text-[clamp(32px,8vw,61px)] text-[#757575] font-medium leading-tight">
             Paid Ads Marketing
