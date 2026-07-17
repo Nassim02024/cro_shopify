6cddaebf2f37cdefdb4c8c4e215e5907086cb533
Nassim <nn1509789@gmail.com>
2026-07-17 21:02:37 +0100

    chan

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,7 +143,7 @@ export default function RootLayout({
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          'https://ss.flaylor-agency.online/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-T8TZRVFG');`}
         </Script>
         {/* End Google Tag Manager */}
@@ -169,7 +169,7 @@ export default function RootLayout({
         <Nav />
         {/* Google Tag Manager (noscript) */}
           <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8TZRVFG"
+          <iframe src="https://ss.flaylor-agency.online/ns.html?id=GTM-T8TZRVFG"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
           </noscript>
         {/* End Google Tag Manager (noscript) */}
